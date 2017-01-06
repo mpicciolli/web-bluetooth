a5d499b9cc5bca671940b792c7cd23b3a06d3be1
mpicciolli <mpiciolli@sovinty.com>
2017-01-06 13:34:44 +0100

    change event

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 /**
  * Created by mpicciolli on 06/01/2017.
  */
-(function () {
-    'use strict';
 
-    var scanBtn = document.getElementById("scanBtn");
+var scanBtn = document.getElementById("scanBtn");
 
-    scanBtn.addEventListener('pointerup', function (event) {
-        // Call navigator.bluetooth.requestDevice
+scanBtn.addEventListener('click', function (event) {
+    // Call navigator.bluetooth.requestDevice
 
-        navigator.bluetooth.requestDevice({filters: [{services: ['battery_service']}]})
-            .then(device => device.gatt.connect())
-            .then(server => {
-                // Getting Battery Service...
-                return server.getPrimaryService('battery_service');
-            })
-            .then(service => {
-                // Getting Battery Level Characteristic...
-                return service.getCharacteristic('battery_level');
-            })
-            .then(characteristic => {
-                // Reading Battery Level...
-                return characteristic.readValue();
-            })
-            .then(value => {
-                console.log('Battery percentage is ' + value.getUint8(0));
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    });
+    navigator.bluetooth.requestDevice({filters: [{services: ['battery_service']}]})
+        .then(device => device.gatt.connect())
+        .then(server => {
+            // Getting Battery Service...
+            return server.getPrimaryService('battery_service');
+        })
+        .then(service => {
+            // Getting Battery Level Characteristic...
+            return service.getCharacteristic('battery_level');
+        })
+        .then(characteristic => {
+            // Reading Battery Level...
+            return characteristic.readValue();
+        })
+        .then(value => {
+            console.log('Battery percentage is ' + value.getUint8(0));
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
